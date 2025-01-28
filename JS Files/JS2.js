2de70476ce5705f081226fe9d082b8c1df429439
MaineAzleiMEstrellan <maineazlei.estrellan@smc.pshs.edu.ph>
2025-01-28 20:22:15 +0800

    Delete JS Files directory
    deleted folder

--- JS Files/JS2.js
+++
@@ -1,7 +0,0 @@
-
-var firstname = prompt("Enter your first name: ");
-var lastname = prompt("Enter your last name: ");
-var birthyear = prompt("Enter your birthyear: ");
-var age=2025-birthyear;
-
-document.getElementById("display").innerHTML = "Hello "+firstname+" "+lastname+"! How does it feel to be "+age+" years old?";
