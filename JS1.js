df9af0a33c6114391fe75350ea5df9c19a775fce
MaineAzleiMEstrellan <maineazlei.estrellan@smc.pshs.edu.ph>
2025-01-28 20:23:41 +0800

    Add files via upload

---
+++ JS1.js
@@ -0,0 +1,12 @@
+
+var nickname = "Maine";
+var heightIn = 60;
+var weightKg = 60;
+
+var heightFeet = heightIn / 12;
+var heightInches = heightIn % 12;
+var weightLbs = weightKg * 2.2046;
+
+alert("Name: " + nickname + "\n" + "Height: " + heightFeet + "\'" + heightInches + "\"" +"\n"+ "Weight: " + weightLbs + " lbs");
+
+
